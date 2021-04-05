19567df6cf1847c3bf7bb27e3f84d45786df648b
cohlerCoded <jeffcohler@cohlercoded.com>
2021-04-05 14:20:34 -0700

    put standard html into my functional component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import ReactDOM from "react-dom";
 // Create react component
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <label for="name" class="label">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style="background-color: blue; color: white">Submit</button>
+    </div>
+  );
 };
 
 // Show react component on the screen
